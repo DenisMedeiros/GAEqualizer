6a4b2101073b5cd64db505e36f7790c6292c7edc
Denis Ricardo da Silva Medeiros <denis@dimap.ufrn.br>
2018-12-01 15:54:27 -0300

    Receiver working.

--- comm.py
+++ comm.py
@@ -5,28 +5,28 @@ import matplotlib.pyplot as plt
 
 class Transmitter:
 
-    def __init__(self, symbols):
-        self.symbols = symbols
-        self.bps = len(list(symbols.keys())[0])
+    def __init__(self, symbols_table):
+        self.symbols_table = symbols_table
+        self.bps = len(list(symbols_table.keys())[0])  # bit per symbol.
 
     '''
     Converts a sequence of bits to a sequence of symbols, defined in SYMBOLS.
     '''
-    def bits2symbols(self, bits):
+    def process(self, bits):
 
         # If the num of bits is not multiple of bps, discard the last bits.
-        nbits = len(bits)
+        n_bits = len(bits)
 
-        if nbits % self.bps != 0:
-            remainder = nbits % self.bps
-            bits = bits[0:nbits-remainder:]
-            nbits -= remainder
+        if n_bits % self.bps != 0:
+            remainder = n_bits % self.bps
+            bits = bits[0:n_bits-remainder:]
+            n_bits -= remainder
 
-        nsymbols = int(nbits / self.bps)
-        symbols = np.empty(nsymbols, dtype=complex)
-        for i in np.arange(0, nbits, self.bps):
+        n_symbols = int(n_bits / self.bps)
+        symbols = np.empty(n_symbols, dtype=complex)
+        for i in np.arange(0, n_bits, self.bps):
             key = bits[i:i + self.bps:1]
-            symbols[int(i/self.bps)] = self.symbols[key]
+            symbols[int(i/self.bps)] = self.symbols_table[key]
 
         return symbols
 
@@ -57,14 +57,15 @@ class Transmitter:
                         fontsize='12'
                         )
 
-        plt.show(block=False)
+        plt.show()
 
 
 class Channel:
 
-    def __init__(self, snr, ntaps, fd):
+    def __init__(self, snr, n_taps, i_delay, fd):
         self.snr = snr
-        self.ntaps = ntaps
+        self.n_taps = n_taps
+        self.i_delay = i_delay
         self.fd = fd
 
     '''
@@ -73,29 +74,82 @@ class Channel:
     def apply_awgn(self, symbols):
 
         # Calculate the average signal power.
-        symbolsp = np.sum(np.square(np.abs(symbols))) / symbols.size
+        symbols_power = np.sum(np.square(np.abs(symbols))) / symbols.size
 
         # Generate the complex AWGN (mean = 0, variance = noisep).
-        noisep = symbolsp / self.snr
+        noise_power = symbols_power / self.snr
 
         # Since there are the real and imaginary part, the average power of
         # the noise must be divided by 2.
-        std = np.sqrt(noisep/2)
+        std = np.sqrt(noise_power/2)
 
         awgnr = np.random.normal(0, std, symbols.size)
         awgni = 1j*np.random.normal(0, std, symbols.size)
         awgn = awgnr + awgni
 
         # Apply the noise to the signal.
-        symbolsn = symbols + awgn
+        symbols_n = symbols + awgn
 
-        return symbolsn
-
+        return symbols_n
 
     def process(self, symbols):
-        pass
+
+        # Model for Rayleigh fading channel
+        delay = np.zeros(self.i_delay, dtype=complex)
+        h = np.append(delay,
+                np.random.randn(self.n_taps) + 1j * np.random.randn(self.n_taps))
+
+        h = 1
+
+        # Impulse response of the channel.
+        y = np.convolve(symbols, h)
+
+        # Apply AWGN noise.
+        #symbols_c = self.apply_awgn(y)
+        symbols_c = y
+
+        return symbols_c
 
 
 
 class Receiver:
-    pass
+
+    def __init__(self, transmitter):
+
+        symbols_table = transmitter.symbols_table
+
+        self.bps = len(list(symbols_table.keys())[0])
+
+        # Generate the points of the constellation.
+        self.symbols_points = list(symbols_table.values())
+
+        # Convert each block of bits to an array of chars.
+
+        n_symbols = len(symbols_table)
+        bits_blocks = list(symbols_table.keys())
+
+        self.symbols_bits = np.empty((n_symbols, self.bps), dtype=str)
+        for i in np.arange(0, n_symbols, 1):
+            self.symbols_bits[i, :] = np.array(list(bits_blocks[i]))
+
+
+    '''Makes a decision and choose a symbol.'''
+    def process(self, symbols_c):
+
+        # Create a empty vector for each bit.
+        bits = np.full(symbols_c.size * self.bps, 'x', dtype=str)
+
+        k = 0
+        for i in np.arange(0, symbols_c.size, 1):
+            # Get the current symbol.
+            symbol = symbols_c[i]
+            # Calculate the euclidean distances from the constellations points.
+            euclidean_distance = np.abs(symbol - self.symbols_points)
+
+            # Choose the lower distance and get the related bits.
+            chosen_pos = np.argmin(euclidean_distance)
+            chosen_bits = self.symbols_bits[chosen_pos]
+            bits[k:k+self.bps:1] = chosen_bits
+            k += self.bps
+
+        return ''.join(bits)
--- simulator.py
+++ simulator.py
@@ -6,52 +6,64 @@ import matplotlib.pyplot as plt
 from comm import Transmitter, Channel, Receiver
 
 
-# Configuration.
-
-SNRdB = 7  # Signal-to-Noise ratio in dB.
+''' Configuration. '''
+SNRdB = 100  # Signal-to-Noise ratio in dB.
 SNR = 10.0 ** (SNRdB/10.0)
-NTAPS = 5  # Number of taps in the multipath channel.
+NTAPS = 1  # Number of taps in the multipath channel.
+INITIAL_DELAY = 0  # Initial delay (number of symbols).
 FD = 5  # Doppler frequency of the channel.
 
 # Symbols table following gray code sequence.
-SYMBOLS1 = {
-    '00': -1 - 1j,
-    '01': -1 + 1j,
-    '11': 1 + 1j,
-    '10': 1 - 1j,
+SYMBOLS_TABLE1 = {
+    '00': 1 * np.exp(1j * np.radians(45)),
+    '01': 1 * np.exp(1j * np.radians(135)),
+    '11': 1 * np.exp(1j * np.radians(225)),
+    '10': 1 * np.exp(1j * np.radians(315)),
 }
 
-SYMBOLS2 = {
-    '000': 1 * np.exp(1j * np.radians(0)),
-    '001': 1 * np.exp(1j * np.radians(45)),
-    '011': 1 * np.exp(1j * np.radians(90)),
-    '010': 1 * np.exp(1j * np.radians(135)),
-    '110': 1 * np.exp(1j * np.radians(180)),
-    '111': 1 * np.exp(1j * np.radians(225)),
-    '101': 1 * np.exp(1j * np.radians(270)),
-    '100': 1 * np.exp(1j * np.radians(305)),
+SYMBOLS_TABLE2 = {
+    '000': 1 * np.exp(1j * np.radians(22.5)),
+    '001': 1 * np.exp(1j * np.radians(67.5)),
+    '011': 1 * np.exp(1j * np.radians(112.5)),
+    '010': 1 * np.exp(1j * np.radians(157.5)),
+    '110': 1 * np.exp(1j * np.radians(202.5)),
+    '111': 1 * np.exp(1j * np.radians(247.5)),
+    '101': 1 * np.exp(1j * np.radians(292.5)),
+    '100': 1 * np.exp(1j * np.radians(337.5)),
 }
 
 
 # Creation of the transmitter, channel, and receiver.
-transmitter = Transmitter(SYMBOLS1)
-channel = Channel(SNR, NTAPS, FD)
-receiver = Receiver()
+transmitter = Transmitter(SYMBOLS_TABLE1)
+channel = Channel(SNR, NTAPS, INITIAL_DELAY, FD)
+receiver = Receiver(transmitter)
 
 
-# Simulation.
+'''Simulation'''
+bits = '00011011'
 
-bits = '010010000'
-symbols = transmitter.bits2symbols(bits)
-symbolsn = channel.apply_awgn(symbols)
+# Transmitter codification.
+symbols = transmitter.process(bits)
 
-transmitter.plot_symbols()
+# Channel processing.
+symbols_c = channel.process(symbols)
 
-print(symbols)
-print(symbolsn)
+# Receiver decodifitation.
+bits_r = receiver.process(symbols_c)
 
-#plt.plot(symbols)
-#plt.plot(symbolsn)
+print('Bits sent:     {} '.format(bits))
+print('Bits received: {} '.format(bits_r))
 
-input()
+erros = np.equal(bits, bits_r)
+print(erros)
+
+#print('bits sent')
+#print(bits)
+#print('symbols received')
+#print(bits_r)
+
+#transmitter.plot_symbols()
+#print(symbols)
+#print(symbolsn)
+
 
